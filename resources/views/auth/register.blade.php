92f74d92d7e80dfe16431f583b032f3b5d09b256
Miyagi-de-engineer <t.hayato15@gmail.com>
2021-02-28 21:30:27 +0900

    Laravel-sns-memo アプリケーションを作成しました。
    N＋1問題にも少しだけ取り掛かりました。

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,6 +14,8 @@
                 <div class="card-body text-center">
                     <h2 class="h3 card-title text-content mt-2">ユーザー登録</h2>
 
+                    <a href="{{ route('login.{provider}',['provider' => 'google']) }}" class="btn btn-block btn-danger"><i class="fab fa-google mr-1"></i>Googleで登録</a>
+
                     @include('error_card_list')
 
                     <div class="card-text">
